f81418f77e81d54a370510e8fde92ee6ec387a6c
maryna-t <43456771+maryna-t@users.noreply.github.com>
2018-12-28 14:07:44 +0200

    mvvmCross added

---
+++ GeoStat-Mobile/GeoStat.Common/App.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MvvmCross.IoC;
+using MvvmCross.ViewModels;
+using GeoStat.Common.ViewModels;
+
+namespace GeoStat.Common
+{
+    public class App : MvxApplication
+    {
+        public override void Initialize()
+        {
+            CreatableTypes()
+                .EndingWith("Service")
+                .AsInterfaces()
+                .RegisterAsLazySingleton();
+            RegisterAppStart<HomeViewModel>();
+        }
+    }
+}
---
+++ GeoStat-Mobile/GeoStat.Droid/Views/HomeView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using GeoStat.Common.ViewModels;
+using MvvmCross.Platforms.Android.Presenters.Attributes;
+using MvvmCross.Platforms.Android.Views;
+
+namespace GeoStat.Droid.Views
+{
+    [MvxActivityPresentation]
+    [Activity(Label = "View for HomeViewModel")]
+    public class HomeView : MvxActivity<HomeViewModel>
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.HomeView);
+        }
+    }
+}
