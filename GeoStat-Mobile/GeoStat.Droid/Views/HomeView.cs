bf7c074390b6a940a0be0d8752bed70a81700f7f
Artem Marakhovskyi <Artem.marakhovskyi@gmail.com>
2019-02-01 13:28:03 +0200

    refactoring

--- GeoStat-Mobile/GeoStat.Droid/Views/HomeView.cs
+++ GeoStat-Mobile/GeoStat.Droid/Views/HomeView.cs
@@ -1,22 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Android;
-using Android.App;
-using Android.Content;
+﻿using Android.App;
 using Android.OS;
-using Android.Runtime;
-using Android.Util;
-using Android.Views;
-using Android.Widget;
 using GeoStat.Common.ViewModels;
 using MvvmCross.Platforms.Android.Presenters.Attributes;
-using Android.Support.V4.App;
-using Android.Support.Design.Widget;
-using MvvmCross.Platforms.Android.Views;
-using Android.Content.PM;
-using Plugin.Permissions;
 
 namespace GeoStat.Droid.Views
 {
@@ -31,12 +16,5 @@ namespace GeoStat.Droid.Views
 
             Microsoft.WindowsAzure.MobileServices.CurrentPlatform.Init();
         }
-
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-        }
-
     }
 }
