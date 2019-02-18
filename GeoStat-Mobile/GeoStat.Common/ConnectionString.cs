3f78fb60abc253a5298ff67f0491b366d780f0ab
Sabina Pashayeva <sabipash270599@gmail.com>
2019-02-18 22:36:47 +0200

    add directives

--- GeoStat-Mobile/GeoStat.Common/ConnectionString.cs
+++ GeoStat-Mobile/GeoStat.Common/ConnectionString.cs
@@ -3,6 +3,10 @@ namespace GeoStat.Common
 {
     public class ConnectionString
     {
+#if ALPHA
         public static string BackendUri { get; } = "http://geostat-alpha.azurewebsites.net";
+#elif BETA
+        public static string BackendUri { get; } = "http://geostat-app.azurewebsites.net";
+#endif
     }
 }
